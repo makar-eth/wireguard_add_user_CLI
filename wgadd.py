1d33b11e279b9268cf6fdf694f521728be8110a1
makar.eth <makar.ananyin@yandex.ru>
2022-08-07 13:50:16 +0300

    for testing

--- wgadd.py
+++ wgadd.py
@@ -7,6 +7,11 @@ from config import *
 @click.argument('username', type=str)
 def main(username):
     create_users_keys(username)
+    publickey = subprocess.check_output(list(f"cat {username}_publickey".split()))
+    privatekey = subprocess.check_output(list(f"cat {username}_privatekey".split()))
+    add_user_to_conf(publickey)
+    wireguard_reboot()
+    print_user_data(privatekey)
 
 def create_users_keys(username):
     if os.path.exists(f"{username}_privatekey") or os.path.exists(f"{username}_publickey"):
@@ -15,6 +20,30 @@ def create_users_keys(username):
     else:
         os.system(f"wg genkey | tee /etc/wireguard/{username}_privatekey | wg pubkey | tee /etc/wireguard/{username}_publickey")
 
+def add_user_to_conf(publickey):
+    conf = open(CONF_PATH, 'a')
+    conf.write(f"\n[Peer] \
+        PublicKey = {publickey} \
+        AllowedIPs = 10.0.0.{NUMBER}/32\n \
+    ")
+    conf.close()
+
+def wireguard_reboot():
+    os.system(f"systemctl restart wg-quick@{CONF_NAME}")
+    os.system(f"systemctl status wg-quick@{CONF_NAME}")
+
+def print_user_data(privatekey):
+    click.echo(f"""\n[Interface]
+        PrivateKey = {privatekey}
+        Address = 10.0.0.{NUMBER}/32
+        DNS = 8.8.8.8
+
+        [Peer]
+        PublicKey = {SERVER_PUBLICKEY}
+        Endpoint = {SERVER_IP}:{PORT}
+        AllowedIPs = 0.0.0.0/0
+        PersistentKeepalive = 20\n
+    """)
 
 if __name__=='__main__':
     main()
