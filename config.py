2a0e3a25c7e3854261a7c743beabcca01216cfd4
makar.eth <makar.ananyin@yandex.ru>
2022-08-07 21:57:51 +0300

    fix requirements to older version of click | delete my info from config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-SERVER_IP= "185.94.165.173" # Внешний ip адрес сервера
-PORT= "51820" # Порт для Wireguard
+SERVER_IP= "" # Внешний ip адрес сервера
+PORT= "" # Порт для Wireguard
 SERVER_PUBLICKEY = ""
 
 NUMBER= "" # Начиная с какого номера создавать пользователей 10.0.0.NUMBER/32
